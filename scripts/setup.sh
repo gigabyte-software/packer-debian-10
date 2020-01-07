586f6ef5d74417e81658a77d5880efd90a0b1d7f
Mike Whiting <mike@gigabyte.software>
2020-01-07 13:10:14 +0000

    proof-of-concept consul host setup (before deciding on packer AMI approach - see environments repo)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 # Disable daily apt unattended updates. Is this needed for Debian?
 # echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
+
+# start docker
+mv /home/vagrant/docker/docker-compose.service /etc/systemd/sysytem
+systemctl enable docker-compose
